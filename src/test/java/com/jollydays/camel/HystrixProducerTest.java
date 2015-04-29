5437bae0b87b541f1dea035417b676bdafcb0542
Roman Mohr <r.mohr@jollydays.com>
2015-04-29 13:57:02 +0200

    Test if open circuit is handled

--- src/test/java/com/jollydays/camel/HystrixProducerTest.java
+++ src/test/java/com/jollydays/camel/HystrixProducerTest.java
@@ -1,5 +1,7 @@
 package com.jollydays.camel;
 
+import com.netflix.config.ConfigurationManager;
+import com.netflix.hystrix.exception.HystrixRuntimeException;
 import org.apache.camel.CamelContext;
 import org.apache.camel.EndpointInject;
 import org.apache.camel.Produce;
@@ -7,9 +9,12 @@ import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Before;
 import org.junit.Test;
 
 public class HystrixProducerTest extends CamelTestSupport {
+
+	public static final String CIRCUIT_BREAKER_FORCE_OPEN = "hystrix.command.default.circuitBreaker.forceOpen";
 
 	@Produce(uri = "direct:start")
 	protected ProducerTemplate template;
@@ -21,9 +26,16 @@ public class HystrixProducerTest extends CamelTestSupport {
 
 		@Override
 		public void configure() throws Exception {
+			onException(HystrixRuntimeException.class).handled(true).setBody().constant("error");
 			from("direct:start").to("hystrix:direct:mitm?groupId=test");
 			from("direct:mitm").to("mock:result");
 		}
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+		ConfigurationManager.getConfigInstance().setProperty(CIRCUIT_BREAKER_FORCE_OPEN, "false");
 	}
 
 	@Override
@@ -42,5 +54,12 @@ public class HystrixProducerTest extends CamelTestSupport {
 		resultEndpoint.expectedBodiesReceived("test");
 		template.sendBody("test");
 		assertMockEndpointsSatisfied();
+	}
+
+	@Test
+	public void shouldReactOnOpenCircuit() throws InterruptedException {
+		ConfigurationManager.getConfigInstance().setProperty(CIRCUIT_BREAKER_FORCE_OPEN, "true");
+		final Object response = template.requestBody("test");
+		assertEquals("error", response);
 	}
 }
